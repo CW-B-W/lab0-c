2a2ad111759ff077b5a2e02968c6a2663029ac42
CW-B-W <cw.brad.wang@gmail.com>
2020-12-28 16:24:57 +0800

    Finished implementing O(1) q_size()
    This method simply return the data member `size` of queue_t to
    achieve O(1) q_size()

--- queue.c
+++ queue.c
@@ -84,6 +84,8 @@ int q_size(queue_t *q)
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (q)
+        return q->size;
     return 0;
 }
 
